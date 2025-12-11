2534f9d61808b52fd7bc032d0eb5cb7ec00d0b50
joshue11 <joshuemm2001@gmail.com>
2025-12-11 19:18:54 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running("03 Nov 2022", 30, 4.8));
+        activities.Add(new Cycling("04 Nov 2022", 45, 20));
+        activities.Add(new Swimming("05 Nov 2022", 30, 40));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+    }
+}
