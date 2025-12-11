63eb8b332c56717b0235591ca5d9573867939deb
joshue11 <joshuemm2001@gmail.com>
2025-12-11 19:19:29 +0100

    Create Cycling.cs

---
+++ Cycling.cs
@@ -0,0 +1,22 @@
+public class Cycling : Activity
+{
+    private double _speedKph;
+
+    public Cycling(string date, int lengthMinutes, double speedKph)
+        : base(date, lengthMinutes)
+    {
+        _speedKph = speedKph;
+    }
+
+    public override double GetDistance()
+    {
+        return _speedKph * (GetLength() / 60.0);
+    }
+
+    public override double GetSpeed() => _speedKph;
+
+    public override double GetPace()
+    {
+        return 60 / _speedKph;
+    }
+}
