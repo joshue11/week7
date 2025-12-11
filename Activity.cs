79ff8dbd697c20c7f53a390e449cdcde7641d80c
joshue11 <joshuemm2001@gmail.com>
2025-12-11 19:20:04 +0100

    Create Activity.cs

---
+++ Activity.cs
@@ -0,0 +1,28 @@
+using System;
+
+public abstract class Activity
+{
+    private string _date;
+    private int _lengthMinutes;
+
+    public Activity(string date, int lengthMinutes)
+    {
+        _date = date;
+        _lengthMinutes = lengthMinutes;
+    }
+
+    public string GetDate() => _date;
+    public int GetLength() => _lengthMinutes;
+
+    public abstract double GetDistance(); 
+    public abstract double GetSpeed();    
+    public abstract double GetPace();     
+
+    public virtual string GetSummary()
+    {
+        return $"{_date} {this.GetType().Name} ({_lengthMinutes} min): " +
+               $"Distance {GetDistance():0.0} km, " +
+               $"Speed {GetSpeed():0.0} kph, " +
+               $"Pace: {GetPace():0.00} min per km";
+    }
+}
